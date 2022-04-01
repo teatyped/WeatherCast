0f37d52c6e8a943141714c61a5def5ce181f2d72
Khai Truong <kminht@gmail.com>
2022-04-01 16:29:12 -0400

    display 5 day cast

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ var currentContainer = document.getElementById("current-weather");
 var cityTitle = document.getElementById("city-name");
 var forecastDays = document.getElementById("forecast-data");
 var userFromEl = document.getElementById("user-form");
-
+var forecastContainer = document.getElementById("forecast-container");
 
 const date = new Date();
 const month = date.getMonth() + 1;
@@ -10,79 +10,93 @@ const day = date.getDate();
 const year = date.getFullYear();
 const fullYear = " " + "(" + month + "/" + day + "/" + year + ")";
 
-
-document.getElementById('button').addEventListener('click', handleClick);
+document.getElementById("button").addEventListener("click", handleClick);
 
 function handleClick(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    console.log("in click fun");
+  console.log("in click fun");
 
-    let city = document.getElementById('userInput').value.trim();
-    console.log("user input " + city);
+  let city = document.getElementById("userInput").value.trim();
+  console.log("user input " + city);
 
-    if (city) {
-        let url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${api_key}`
-        console.log("user URL " + url);
+  if (city) {
+    let url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${api_key}`;
+    console.log("user URL " + url);
 
-        fetch(url)
-            .then(data => data.json())
-            .then(data => {
-                const { lat, lon, name } = data[0];
+    fetch(url)
+      .then((data) => data.json())
+      .then((data) => {
+        const { lat, lon, name } = data[0];
 
-                console.log(data[0]);
-                
-                cityTitle.innerHTML =
-                    `<h3 class="current-city card-title">${name + fullYear}</h3>`
+        console.log(data[0]);
 
+        cityTitle.innerHTML = `<h3 class="current-city card-title">${
+          name + fullYear
+        }</h3>`;
 
-                let url2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${api_key}`
-                fetch(url2)
-                    .then(data => data.json())
-                    .then(data => {
-                        const { temp, wind_speed, humidity, uvi } = data.current;
-                        currentContainer.innerHTML =
-                            `<div class="current-data card-body">
-                                <div class="card-text">Temp: ${temp}°F</div>
-                                <div class="card-text">Wind: ${wind_speed}mph</div>
-                                <div class="card-text">Humidity: ${humidity}%</div>
-                                <div class="card-text">UV Index: ${uvi}</div>
-                            </div>`;
+        let url2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${api_key}`;
+        fetch(url2)
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+            // pass data and display info to document
+            displayCurrent(data);
 
+            for (var i = 1; i <= 5; i++) {
+              var weatherData = {
+                date: data.daily[i].dt,
+                temp: data.daily[i].temp.day,
+                wind_speed: data.daily[i].wind_speed,
+                humidity: data.daily[i].humidity,
+                icon: data.current.weather[0].icon
+              };
 
-                        // for (var i = 1; i <= 5; i++) {
-                        //     const temp = data.daily[i].temp.day;
-                        //     const { dt, wind_speed, humidity, uvi } = data.daily[i];
-                        //     console.log(data.daily[i]);
+              var currentDate = moment
+                .unix(weatherData.date)
+                .format("MM/DD/YYYY");
 
-                        //     const milliseconds = dt * 1000;
-                        //     const dateObject = new Date(milliseconds);
-                        //     const humanDate = dateObject.toLocaleString("en-US", { timeZoneName: "short" });
-                        //     const splits1 = humanDate.split(", ")[0];
-
-                        //     forecastDays.innerHTML =
-                        //         `<div>
-                        //             <div>Temp: ${temp}°F</div>
-                        //             <div>Wind: ${wind_speed}mph</div>
-                        //             <div>Humidity: ${humidity}%</div>
-                        //             <div>UV Index: ${uvi}</div>
-                        //         </div>`;
-                        // }
-                    }
-                    )
-            })
-    } else if (!city) {
-        alert("Please enter a city.");
-    }
-};
-
-
-function displayCurrent (){
-
+            
+                document.getElementById("forecast-" + i).innerHTML = 
+            
+                                `<div class="card" id="day-card">
+                                    <div class = "card-body">
+                                        <h5 class = "card-title">${currentDate}</h4>
+                                        <div><img src="http://openweathermap.org/img/w/${weatherData.icon}.png"></div>
+                                        <div class = "card-text">Temp: ${weatherData.temp}°F</div>
+                                        <div class = "card-text">Wind: ${weatherData.wind_speed}mph</div>
+                                        <div class = "card-text">Humidity: ${weatherData.humidity}%</div>
+                                    </div>    
+                                </div>`;
+            }
+          });
+      });
+  } else if (!city) {
+    alert("Please enter a city.");
+  }
 }
 
-function displayForcast(){
+function displayCurrent(data) {
 
+  const {icon} = data.current.weather[0];
+  const { temp, wind_speed, humidity, uvi } = data.current;
+
+  var colorBlock = ""
+  if (uvi > 7) {
+      colorBlock = "bg-danger";
+  } else if (uvi > 4 && uvi < 7) {
+      colorBlock = "bg-warning";
+  } else if (uvi < 4) {
+      colorBlock = "bg-success";
+  }
+  currentContainer.innerHTML = 
+  
+    `<div class="current-data card-body">
+        <img src="http://openweathermap.org/img/w/${icon}.png">
+            <div class="card-text">Temp: ${temp}°F</div>
+            <div class="card-text">Wind: ${wind_speed}mph</div>
+            <div class="card-text">Humidity: ${humidity}%</div>
+            <div class="card-text ${colorBlock}">UV Index: ${uvi}</div>
+        </div>`;
 }
-
 
