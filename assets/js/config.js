2345db6df3121cc1aa4eaa40c633ad290b0bc07c
Khai Truong <kminht@gmail.com>
2022-04-01 13:39:54 -0400

    adde script files

---
+++ assets/js/config.js
@@ -0,0 +1,1 @@
+const api_key = "f231beb86da75f0718532350ac3d5f17"
---
+++ assets/js/script.js
@@ -0,0 +1,73 @@
+var currentContainer = document.getElementById("current-weather");
+var cityTitle = document.getElementById("city-title");
+var forecastDays = document.getElementById("forecast-data");
+
+const date = new Date();
+const month = date.getMonth() + 1;
+const day = date.getDate();
+const year = date.getFullYear();
+const fullYear = " " + "(" + month + "/" + day + "/" + year + ")";
+
+document.querySelector('button').addEventListener('click', handleClick);
+
+function handleClick() {
+    let city = document.getElementById('userInput').value.trim();
+    if (city) {
+        let url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${api_key}`
+        fetch(url)
+            .then(data => data.json())
+            .then(data => {
+                const { lat, lon, name } = data[0];
+
+                console.log(data[0]);
+                
+                cityTitle.innerHTML =
+                    `<h3 class="current-city">${name + fullYear}</h3>`
+                let url2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${api_key}`
+                fetch(url2)
+                    .then(data => data.json())
+                    .then(data => {
+                        const { temp, wind_speed, humidity, uvi } = data.current;
+                        currentContainer.innerHTML =
+                            `<div class="current-data">
+                                <div>Temp: ${temp}°F</div>
+                                <div>Wind: ${wind_speed}mph</div>
+                                <div>Humidity: ${humidity}%</div>
+                                <div>UV Index: ${uvi}</div>
+                            </div>`;
+
+
+                        for (var i = 1; i <= 5; i++) {
+                            const temp = data.daily[i].temp.day;
+                            const { dt, wind_speed, humidity, uvi } = data.daily[i];
+                            console.log(data.daily[i]);
+
+                            const milliseconds = dt * 1000;
+                            const dateObject = new Date(milliseconds);
+                            const humanDate = dateObject.toLocaleString("en-US", { timeZoneName: "short" });
+                            const splits1 = humanDate.split(", ")[0];
+
+                            forecastDays.innerHTML =
+                                `<div>
+                                    <div>Temp: ${temp}°F</div>
+                                    <div>Wind: ${wind_speed}mph</div>
+                                    <div>Humidity: ${humidity}%</div>
+                                    <div>UV Index: ${uvi}</div>
+                                </div>`;
+                        }
+                    }
+                    )
+            })
+    } else if (!city) {
+        alert("Please enter a city.");
+    }
+};
+
+
+function displayCurrent (){
+
+}
+
+function displayForcast(){
+    
+}
